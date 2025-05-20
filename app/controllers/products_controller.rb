727e848fb9207a4ac86a12c0dbf0441cee9c0ab9
bshin27 <125755440+bshin27@users.noreply.github.com>
2025-05-19 19:54:13 -0500

    Add products discount and tax.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,40 @@
 class ProductsController < ApplicationController
-  def all_products
-    product
+  def index
+    @products = Product.all
+
+    render :index
+  end
+
+  def show
+    @product = Product.find(params[:id])
+
+    render :show
+  end
+
+  def create
+    product = Product.create(
+      name: params[:name],
+      price: params[:price],
+      image_url: params[:image_url],
+      description: params[:description]
+    )
     render json: product
+  end
+
+  def update
+    product = Product.find(params[:id])
+    product.update(
+      name: params[:name],
+      price: params[:price],
+      image_url: params[:image_url],
+      description: params[:description]
+    )
+    render json: product
+  end
+
+  def destroy
+    product = Product.find(params[:id])
+    product.destroy
+    render json: { message: "Product deleted!" }
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  get "/all_products" => "products#all_products"
-  get "first_product" => "products#first_product"
+  # get "/all_products" => "products#all_products"
+  # get "/product/:id" => "products#show"
+  get "/products" => "products#index"
+  get "/products/:id" => "products#show"
+  post "/products" => "products#create"
 end
